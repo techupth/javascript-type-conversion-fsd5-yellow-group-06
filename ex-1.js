3b95d3dfb4f6d5582730a306ea459634483f79aa
kaweeracha <kaweeracha@MacBook-Air-khxng-kaweeracha.local>
2023-11-24 12:32:18 +0700

    fix:exer1

--- ex-1.js
+++ ex-1.js
@@ -1,6 +1,6 @@
 let accountBalance = 400;
 let depositAmountFromUser = "10";
-let depositAmount = parseInt(depositAmountFromUser);
+depositAmountFromUser = Number(depositAmountFromUser);
 accountBalance = accountBalance + depositAmountFromUser;
 let accountBalanceMessage = "Your account balance is " + accountBalance;
 
